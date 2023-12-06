eeab19a6f2a075e78b2bf89a25d0fe86f040222e
Nazar Holodiuk <98541354+Lekantrop-gd@users.noreply.github.com>
2023-12-06 23:40:29 +0200

    Fix bug with coincidence of shapes and forms

--- Little Genuis/Assets/Scripts/Move.cs
+++ Little Genuis/Assets/Scripts/Move.cs
@@ -1,59 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    public GameObject correctForm;
-    private bool moving;
-    private float startPosX;
-    private float startPosY;
-    private bool finish;
-    private Vector3 resetPosition;
+    [SerializeField] private Transform _form;
+    private Vector2 _startPosition;
+    private Vector2 _resetPosition;
+    private bool _isMoving;
+    private bool _isLevelComplete;
 
-    void Start()
+    void Awake()
     {
-        resetPosition = this.transform.localPosition;
+        _resetPosition = transform.position;
     }
 
     void Update()
     {
-        if (finish == false)
+        if (_isLevelComplete == false && _isMoving)
         {
-            if (moving)
-            {
-                Vector3 mousePos;
-                mousePos = Input.mousePosition;
-                mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-                this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, this.gameObject.transform.localPosition.z);
-            }
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector2(mousePosition.x - _startPosition.x, mousePosition.y - _startPosition.y);
         }
     }
 
     private void OnMouseDown()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && _isLevelComplete == false)
         {
-            Vector3 mousePos;
-            mousePos = Input.mousePosition;
-            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-            startPosX = mousePos.x - transform.localPosition.x;
-            startPosY = mousePos.y - transform.localPosition.y;
-            moving = true;
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            _startPosition = mousePosition - (Vector2)transform.position;
+            _isMoving= true;
         }
     }
 
     private void OnMouseUp()
     {
-        moving = false;
-        if (Mathf.Abs(transform.localPosition.x - correctForm.transform.localPosition.x) <= 0.5f && Mathf.Abs(transform.localPosition.y - correctForm.transform.localPosition.y) <= 0.5f)
+        if (_isLevelComplete == false)
         {
-            transform.position = new Vector3(correctForm.transform.position.x, correctForm.transform.position.y, correctForm.transform.position.z);
-            finish = true;
+            if (Mathf.Abs(transform.position.x - _form.position.x) <= 0.5f && 
+                Mathf.Abs(transform.position.y - _form.position.y) <= 0.5f)
+            {
+                transform.position = _form.position;
+                _isLevelComplete = true;
+            }
+            else
+            {
+                transform.localPosition = _resetPosition;
+            }
         }
-        else
-        {
-            transform.localPosition = new Vector3(resetPosition.x, resetPosition.y, resetPosition.z);
-        }
+
+        _isMoving = false;
     }
 }
